9368744e39ec8fe06c08ef3ba8bc54f59f029528
Your Name <you@example.com>
2020-12-13 17:18:48 -0500

    1

--- INIT/octopize/installers/setup.sh
+++ INIT/octopize/installers/setup.sh
@@ -1,6 +1,11 @@
+echo $(date) RAN >> /var/log/octopize/init_sh.log
+
+
 sudo apt update
 sudo apt upgrade -y
 
+
+sudo apt install snapd -y && sudo reboot
 
 snap install microk8s
 microk8s start
--- INIT/octopize/octopize_init.sh
+++ INIT/octopize/octopize_init.sh
@@ -1,3 +1,6 @@
 # DERP DERP DO NOTHING YEt
+mkdir /var/log/octopize/
+echo $(date) RAN >> /var/log/octopize/init_sh.log
 
 curl https://raw.githubusercontent.com/exenin/pubinitscripts/master/INIT/octopize/installers/setup.sh | bash -
+ls /var/.octo/.setup_done && cd ~/var/.octo && git@github.com:exenin/pvtinitscripts.git
